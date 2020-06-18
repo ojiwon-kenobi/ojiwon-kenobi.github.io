b0f1e4f44c40810b3627fbce35dadd30c3c80c21
Jiwon Lee <jiwon.lee@berkeley.edu>
2020-06-18 09:56:54 -0500

    work in progress

--- js/cursor.js
+++ js/cursor.js
@@ -1,11 +1,11 @@
 var cursor = true;
-        var speed = 500;
-        setInterval(() => {
-          if(cursor) {
-            document.getElementById('cursor').style.opacity = 0;
-            cursor = false;
-          }else {
-            document.getElementById('cursor').style.opacity = 1;
-            cursor = true;
-          }
-        }, speed);
+var speed = 500;
+setInterval(() => {
+    if(cursor) {
+    document.getElementById('cursor').style.opacity = 0;
+    cursor = false;
+    }else {
+    document.getElementById('cursor').style.opacity = 1;
+    cursor = true;
+    }
+}, speed);
